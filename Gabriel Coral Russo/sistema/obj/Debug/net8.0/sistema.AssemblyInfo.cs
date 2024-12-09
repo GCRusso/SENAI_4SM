ab6f5904ced59a30e7a95862c3ca8687f1d2ee53
Gabriel Russo <gabriel.coral.russo@gmail.com>
2024-12-09 10:53:22 -0300

    att

--- Gabriel Coral Russo/sistema/obj/Debug/net8.0/sistema.AssemblyInfo.cs
+++ Gabriel Coral Russo/sistema/obj/Debug/net8.0/sistema.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d22eb254c1a58d9175bbb7614a6b3f761a83be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b9a408de769d5d9cf884d93056131285552dcd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
